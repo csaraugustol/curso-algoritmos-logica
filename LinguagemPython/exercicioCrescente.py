d6b0f4e9bc5e4e0d13ab84d6ec59f9040dea8bc0
Cesar Augusto <68163997+csaraugustol@users.noreply.github.com>
2021-10-01 15:26:07 -0300

    Update exercicioCrescente.py

--- LinguagemPython/exercicioCrescente.py
+++ LinguagemPython/exercicioCrescente.py
@@ -1,18 +1,18 @@
 # Lê dois números e informa se foi digitado na ordem crescente ou descreste
 
-number1: int
-number2: int
+firstNumber: int
+seccondNumber: int
 
 print("Digite dois números")
-number1 = int(input())
-number2 = int(input())
+firstNumber = int(input())
+seccondNumber = int(input())
 
-while number1 != number2:
-    if number1 > number2:
+while firstNumber != seccondNumber:
+    if firstNumber > seccondNumber:
         print("Decrescente!")
     else:
         print("Crescente!")
 
     print("Digite outros dois números")
-    number1 = int(input())
-    number2 = int(input())
+    firstNumber = int(input())
+    seccondNumber = int(input())
