0c337fd3f838fb48cb528c0730c6296b3ebc57c0
Cesar Augusto <68163997+csaraugustol@users.noreply.github.com>
2021-10-01 15:26:23 -0300

    Update exercicioDiagonalNegativo.py

--- LinguagemPython/exercicioDiagonalNegativo.py
+++ LinguagemPython/exercicioDiagonalNegativo.py
@@ -1,23 +1,23 @@
 # Imprime a diagonal e a quantidade de negativos na matriz
 
-size: int
+matrizSize: int
 countNegatives: int
 
-size = int(input("Qual a ordem da matriz? "))
+matrizSize = int(input("Qual a ordem da matriz? "))
 
-matriz: [[int]] = [[0 for x in range(size)] for x in range(size)]
+matriz: [[int]] = [[0 for x in range(matrizSize)] for x in range(matrizSize)]
 
-for i in range(0, size):
-    for j in range(0, size):
+for i in range(0, matrizSize):
+    for j in range(0, matrizSize):
         matriz[i][j] = float(input(f"Elemento [{i}/{j}]: "))
 
 print("Diagonal principal:")
-for i in range(0, size):
+for i in range(0, matrizSize):
     print(f"{matriz[i][i]} ", end="")
 
 countNegatives = 0
-for i in range(0, size):
-    for j in range(0, size):
+for i in range(0, matrizSize):
+    for j in range(0, matrizSize):
         if matriz[i][j] < 0:
             countNegatives = countNegatives + 1
 
