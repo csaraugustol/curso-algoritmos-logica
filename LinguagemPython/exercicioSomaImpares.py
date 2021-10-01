060208371b652f022690a4ca803aa21ef7122e60
Cesar Augusto <68163997+csaraugustol@users.noreply.github.com>
2021-10-01 15:27:45 -0300

    Update exercicioSomaImpares.py

--- LinguagemPython/exercicioSomaImpares.py
+++ LinguagemPython/exercicioSomaImpares.py
@@ -1,21 +1,21 @@
 # Lê dois números e imprime a soma dos ímpares entre eles
 
-number1: int
-number1: int
+firstNumber: int
+seccondNumber: int
 change: int
 sumNumbers: int
 
 print("Digite dois números: ")
-number1 = int(input())
-number2 = int(input())
+firstNumber = int(input())
+seccondNumber = int(input())
 
-if number1 > number2:
-    change = number2
-    number2 = number1
-    number1 = change
+if firstNumber > seccondNumber:
+    change = seccondNumber
+    seccondNumber = firstNumber
+    firstNumber = change
 
 sumNumbers = 0
-for i in range(number1 + 1, number2 - 1):
+for i in range(firstNumber + 1, seccondNumber - 1):
     if i % 2 != 0:
         sumNumbers += i
 
