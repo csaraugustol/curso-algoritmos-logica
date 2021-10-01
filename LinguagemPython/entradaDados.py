c3b5ecc2d5c35852e7a2072c8e4da652922522b8
Cesar Augusto <68163997+csaraugustol@users.noreply.github.com>
2021-10-01 16:36:42 -0300

    Update entradaDados.py

--- LinguagemPython/entradaDados.py
+++ LinguagemPython/entradaDados.py
@@ -1,17 +1,17 @@
 # Alguns exemplos de entrada de dados
 
 firstSalary: float
-seccondSalary: float
+secondSalary: float
 firstName: str
-seccondName: str
+secondName: str
 age: int
 gender: str
 
 firstName = input("Nome da primeira pessoa: ")
 firstSalary = float(input("Salário da primeira pessoa: "))
 
-seccondName = input("Nome da segunda pessoa: ")
-seccondSalary = float(input("Salário da segunda pessoa: "))
+secondName = input("Nome da segunda pessoa: ")
+secondSalary = float(input("Salário da segunda pessoa: "))
 
 age = int(input("Digite uma idade: "))
 
@@ -19,7 +19,7 @@ gender = input("Digite um sexo(F/M): ")
 
 print(f"Nome 1: {firstName}  ")
 print(f"Salário 1: {firstSalary}")
-print(f"Nome 2: {seccondName}  ")
-print(f"Salário 2: {seccondSalary}")
+print(f"Nome 2: {secondName}  ")
+print(f"Salário 2: {secondSalary}")
 print(f"Idade: {age}")
 print(f"Sexo: {gender}")
